794078af502b41eab1615fdd7ba7af5abdc6a7c8
Sebastian Frosch <sfrosch@lcs.on.ca>
2023-01-25 11:19:07 -0500

    Initial Commit

---
+++ StructureRecap/StructureRecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  StructureRecapApp.swift
+//  StructureRecap
+//
+//  Created by Sebastian Frosch on 25.01.23.
+//
+
+import SwiftUI
+
+@main
+struct StructureRecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
